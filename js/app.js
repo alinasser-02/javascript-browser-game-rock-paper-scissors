3d4267fbe14311c406cb40ade2bb62c386474b8d
Ali Naser <3linassera7med@gmail.com>
2025-06-29 11:10:50 +0300

    create functions

--- js/app.js
+++ js/app.js
@@ -15,5 +15,25 @@ const scisoorBtnEl = document.querySelector("#scissor");
 const resultDisplayEl = document.querySelector("#result-display");
 
 /*-------------------------------- Functions --------------------------------*/
+getComputerChoice = () => {
+  //generate a random number 0-2
+  const randomIdx = Math.floor(Math.random() * choices.length);
 
+  // select the item from the array
+  return choices[randomIdx];
+};
+
+//initialize game state
+play = () => {
+  computerChoice = getComputerChoice();
+  console.log('computer choice:', computerChoice);
+
+  //after updating state, render to html
+  render();
+};
+
+play();
+
+//updates out UI/html directly
+render = () => {};
 /*----------------------------- Event Listeners -----------------------------*/
