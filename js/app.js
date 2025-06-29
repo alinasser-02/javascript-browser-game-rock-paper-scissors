28bdbd9a5e9566297ae39c806f945ff05e7b1bac
Ali Naser <3linassera7med@gmail.com>
2025-06-29 14:14:53 +0300

    html , css , javascript ,readme updates

--- js/app.js
+++ js/app.js
@@ -1,39 +1,65 @@
-/*-------------------------------- Constants --------------------------------*/
+/*------------ Constants ------------------*/
+const choice = ["rock", "paper", "scissors"];
 
-const choices = ["rock", "paper", "scissors"];
-
-/*-------------------------------- Variables --------------------------------*/
-
+/*------------- Variables -----------------*/
+let msg;
 let playerChoice;
 let computerChoice;
-let msg;
 
-/*------------------------ Cached Element References ------------------------*/
+/*------ Cached Element References ---------*/
 const rockBtnEl = document.querySelector("#rock");
 const paperBtnEl = document.querySelector("#paper");
-const scisoorBtnEl = document.querySelector("#scissor");
+const scissorsBtnEl = document.querySelector("#scissors");
 const resultDisplayEl = document.querySelector("#result-display");
 
-/*-------------------------------- Functions --------------------------------*/
-getComputerChoice = () => {
-  //generate a random number 0-2
-  const randomIdx = Math.floor(Math.random() * choices.length);
-
+/*-------- Functions -----------------------*/
+function getComputerChoice() {
+  // generate a random number 0-2
+  const randomIndex = Math.floor(Math.random() * choice.length);
   // select the item from the array
-  return choices[randomIdx];
+  return choice[randomIndex];
+}
+const getPlayerChoice = (event) => {
+  playerChoice = event.target.id;
 };
 
-//initialize game state
-play = () => {
+// initialize game state
+function play(event) {
   computerChoice = getComputerChoice();
-  console.log('computer choice:', computerChoice);
-
-  //after updating state, render to html
+  // after updating state, render to html
   render();
-};
+  getPlayerChoice(event);
+  getComputerChoice();
+  compare();
+  render();
+}
 
-play();
+// updates our UI/html directly
+function render() {
+  resultDisplayEl.textContent = `Computer chose ${computerChoice} and you chose ${playerChoice}. ${msg}`;
+}
 
-//updates out UI/html directly
-render = () => {};
-/*----------------------------- Event Listeners -----------------------------*/
+function compare() {
+  if (playerChoice === computerChoice) {
+    msg = "You tied!";
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    msg = "Congrats! You win!";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    msg = "Congrats! You win!";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    msg = "Congrats! You win!";
+  } else {
+    msg = "You lose! Try again?";
+  }
+}
+function reset(){  playerChoice = null;
+  computerChoice = null;
+  resultDisplayEl.textContent = '' ;
+}
+
+
+/*--------- Event Listeners ----------------*/
+rockBtnEl.addEventListener("click", play);
+paperBtnEl.addEventListener("click", play);
+scissorsBtnEl.addEventListener("click", play);
+document.querySelector('#resetButton').addEventListener('click', resetGame);
